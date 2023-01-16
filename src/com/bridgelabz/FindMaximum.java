633916016b839a07b7f4c1b40f677ed48f99c10b
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-01-16 21:35:10 +0530

    Added:Find the maximum number of given 3 integers

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
-public class FindMaximum {
+public class FindMaximum <T>{
+    public static <T extends Comparable<T>> T maximumInteger(T x, T y, T z){
+        T max= x;
+        if(y.compareTo(max) > 0)
+            max= y;
+        if(z.compareTo(max) > 0)
+            max= z;
+        return max;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Find maximum problem using Generics ");
+        System.out.println("Maximum of three integer is :" +maximumInteger(2,3,5));
+        System.out.println("Maximum of three integer is :" +maximumInteger(5,7,3));
     }
 }
